d2a6cdd8d10e8d068c4a79fbaa7c71f96c8bddcf
Erostin Lushka <erostin@dhcp-wifi-8021x-155-41-8-135.bu.edu>
2017-11-06 17:42:47 -0500

    Added all the files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# RPi_LED_example
-App to controll an LED from phone onto the RPi 3
+Steps to getting this project running on your computer:
+
+1. Git clone
+
+2. cd to project directory
+
+3. Run "ionic serve" on your terminal
+
+4. Edit the home.html in src/pages/home directory 
---
+++ src/pages/home/home.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+
+  }
+
+  showToast(position: string) {
+    let toast = this.toastCtrl.create({
+      message: 'Mmmm, buttered toast',
+      duration: 2000,
+      position: position
+    });
+
+    toast.present(toast);
+  }
+
+  showToastWithCloseButton() {
+    const toast = this.toastCtrl.create({
+      message: 'Your files were successfully saved',
+      showCloseButton: true,
+      closeButtonText: 'Ok'
+    });
+    toast.present();
+  }
+
+  showLongToast() {
+    let toast = this.toastCtrl.create({
+      message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptatibus quibusdam eum nihil optio, ullam accusamus magni, nobis suscipit reprehenderit, sequi quam amet impedit. Accusamus dolorem voluptates laborum dolor obcaecati.',
+      duration: 2000,
+    });
+    toast.present();
+  }
+
+}
