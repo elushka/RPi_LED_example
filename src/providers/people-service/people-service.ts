9687151b76d23b576134db525b6b28406fca77ab
Erostin Lushka <erostin@rfc1918.address.not.used.bu.edu>
2017-11-09 01:43:18 -0500

    Fixed GPIO pin number in the API call in services

--- src/providers/people-service/people-service.ts
+++ src/providers/people-service/people-service.ts
@@ -27,7 +27,7 @@ export class PeopleServiceProvider {
       // We're using Angular HTTP provider to request the data,
       // then on the response, it'll map the JSON data to a parsed JS object.
       // Next, we process the data and resolve the promise with the new data.
-      this.http.get('https://smartmory-pi.onlosant.com/gpio/4.json')
+      this.http.get('https://smartmory-pi.onlosant.com/gpio/17')
         .map(res => res.json())
         .subscribe(data => {
           // we've got back the raw data, now generate the core schedule data
